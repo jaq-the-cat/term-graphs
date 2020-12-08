b1bb0a50f61c264537f6d84f8ce38e690a84d242
jaq-cat <jaq.cat@protonmail.ch>
2020-12-08 11:23:01 -0300

    yay

--- src/line.c
+++ src/line.c
@@ -1,1 +1,24 @@
 #include "line.h"
+
+void Gdraw_line(Data d, size_t len_d) {
+    int x1 = 1, y1 = 1;
+    int x2 = 5, y2 = 5;
+    int dx = (x2 - x1);
+    int dy = (y2 - y1);
+
+    int step;
+    if (abs(dx) >= abs(dy))
+        step = abs(dx);
+    else
+        step = abs(dy);
+
+    dx = dx / step;
+    dy = dy / step;
+    int x = x1;
+    int y = y1;
+    for (int i=0; i < step; i++) {
+        mvaddch(y, x, 'x');
+        x = x + dx;
+        y = y + dy;
+    }
+}
--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
 #include "libs.h"
 #include "render.h"
 #include "bar.h"
+#include "line.h"
 
 int main() {
     GWin scr = Ginit();
 
+    Gdraw_line(LINE(1, 1, 5, 5), 4);
     refresh();
     napms(3000);
 
