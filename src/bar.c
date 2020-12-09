3a330ae80e76bc85afcf8af520e58ad7abffc892
jaq-cat <jaq.cat@protonmail.ch>
2020-12-09 09:39:58 -0300

    max_bar_width

--- src/bar.c
+++ src/bar.c
@@ -10,33 +10,37 @@ char* make_bar(char* label, int w) {
 }
 
 void Gdraw_bar(int gx, int gy, Bar d[], size_t len_b) {
-    int bar_h = 0;
-    for (int i=0; i<len_b; i++)
-        if (d[i].h > bar_h)
-            bar_h = d[i].h;
-    bar_h++;
-    int bar_w = 6;
+    int l;
+    int max_bar_w = 0, max_bar_h = 0;
+    for (int i=0; i<len_b; i++) {
+        if (d[i].h > max_bar_h)
+            max_bar_h = d[i].h;
+        if ((l = strlen(d[i].label)) > max_bar_w)
+            max_bar_w = l;
+    }
+
+    max_bar_h++;
 
     // draw lines
-    for (int y=gy; y<bar_h+gy; y++)
+    for (int y=gy; y<max_bar_h+gy; y++)
         mvaddch(y, gx, '+');
-    for (int x=gx; x<bar_w*2+gx; x+=2)
-        mvaddstr(bar_h+gy, x, "+ ");
+    for (int x=gx; x<max_bar_w*2+gx; x+=2)
+        mvaddstr(max_bar_h+gy, x, "+ ");
     
     const int gap = 1;
     int x = gx + gap*2;
-    int bar_width;
+    int bar_w;
     for (int i=0; i<len_b; i++) {
-        bar_width = strlen(d[i].label) + 4;
+        bar_w= strlen(d[i].label) + 4;
 
         // draw bar sections
         for (int h=0; h<d[i].h; h++) {
-            mvaddch(bar_h+gy-1-h, x, '[');
-            mvaddch(bar_h+gy-1-h, x + bar_width-1, ']');
+            mvaddch(max_bar_h+gy-1-h, x, '[');
+            mvaddch(max_bar_h+gy-1-h, x + bar_w-1, ']');
         }
 
         // draw label
-        mvaddstr(bar_h+gy-1, x + 2, d[i].label);
-        x += bar_width + gap;
+        mvaddstr(max_bar_h+gy-1, x + 2, d[i].label);
+        x += bar_w + gap;
     }
 }
