5fa8c6960952b29b3bc9f375ff26cff4da1738de
jaq-cat <jaq.cat@protonmail.ch>
2020-12-09 14:20:46 -0300

    cursor :P

--- src/render.c
+++ src/render.c
@@ -4,11 +4,13 @@ GWin Ginit() {
     initscr();
     cbreak();
     noecho();
+    curs_set(0);
     return stdscr;
 }
 
 void Gend(GWin stdscr) {
     nodelay(stdscr, false);
+    curs_set(1);
     echo();
     nocbreak();
     endwin();
