35f0a060289192d80b69af7e46a13b6e3faee8a8
jaq-cat <jaq.cat@protonmail.ch>
2020-12-09 09:46:42 -0300

    2 graphs :P

--- src/main.c
+++ src/main.c
@@ -6,22 +6,22 @@
 int main() {
     GWin scr = Ginit();
 
-    /*LineData d = {*/
-        /*1,*/
-        /*15,*/
-        /*5,*/
-        /*25,*/
-    /*};*/
-    /*Gdraw_line(1, 1, d, 4, 6, "X label", "Y label");*/
+    LineData ld = {
+        1,
+        15,
+        5,
+        25,
+    };
+    Gdraw_line(1, 1, ld, 4, 6, "X label", "Y label");
 
-    Bar d[] = {
+    Bar bd[] = {
         BAR(10, "Pyjamas"),
         BAR(15, "Cereal"),
         BAR(8, "Violins"),
         BAR(5, "Cars"),
         BAR(2, "Mugs"),
     };
-    Gdraw_bar(1, 1, d, 5);
+    Gdraw_bar(1, 32, bd, 5);
 
     refresh();
     getch();
